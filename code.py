5e948d5e9ce7940136051e21fedc2166e8816c66
GeorgeXu12245 <george_xu@berkeley.edu>
2026-04-23 21:43:36 -0700

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,26 @@ import digitalio
 import neopixel
 import rotaryio
 import pwmio
+from analogio import AnalogIn
+# adc 
+l_adc = analogio.AnalogIn(board.GP28)
+#"""TODO call AnalogIn on GP28"""
 
+# emitter 
+l_en = digitalio.DigitalInOut(board.GP7)
+l_en.direction = digitalio.Direction.OUTPUT
+l_en.value = False
+
+# sensors
+lir_a = digitalio.DigitalInOut(board.GP5)
+lir_a.direction = digitalio.Direction.OUTPUT
+lir_a.drive_mode = digitalio.DriveMode.OPEN_DRAIN
+lir_a.value = True # high Z mode
+
+lir_b = digitalio.DigitalInOut(board.GP6)
+lir_b.direction  = digitalio.Direction.OUTPUT
+lir_b.drive_mode = digitalio.DriveMode.OPEN_DRAIN
+lir_b.value = True
 # from ds28e05  import DS28E05
 from irsensor import IRSensors
 import adafruit_motor.motor as motor
@@ -54,8 +73,25 @@ if __name__ == "__main__":
     enc = rotaryio.IncrementalEncoder(board.GP12, board.GP13)
 
     while True:
-        print(enc.position)
+    # TODO enable IR emitters using l_en set one of its variables to a (boolean)
+        l_en.value = True
+    # TODO enable chosen sensor lir_a or lir_b by setting one of its variable to false
+        lir_a.value = False
+    # TODO wait a bit, 1ms should do time. ...
         time.sleep(0.001)
+    # TODO take analog reading for future printing print(l_adc. , end=" ")
+        #print("l_adc.value:", l_adc.value, end = " ")
+        #print("lir_a.value:", lir_a.value, end = " ")
+        print("HI")
+    # TODO disable chosen sensor lir_a or lir_b 
+        lir_a.value = True
+    # TODO try for lir_b or lir_a whichever one you didnt use instead, enable chosen sensor etc
+
+    # Repeat above for this sensor
+
+    # TODO disable IR emitters using l_en 
+        l_en.value = False
+    time.sleep(0.05)
 
     # Polling - Lab 3
     # # a and b Hall effect sensors of left encoder
